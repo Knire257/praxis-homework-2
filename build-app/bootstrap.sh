0caf32bb32a6d43c17b046dcbb9a029a671cfcca
Daniel Naranjo <danaranjor@unal.edu.co>
2021-10-31 22:42:11 -0500

    First commit: installing libraries

--- build-app/bootstrap.sh
+++ build-app/bootstrap.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
-echo "Hello world!"
+
+#Installing git
+sudo yum install git
+git version
+
+#Installing Go
+sudo yum install golang
+go version
+
+#Installing node and npm
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install nodejs
+node --version
+npm --version
+
+#Installing vue cli
+sudo npm install -g -y @vue/cli
+vue --version
+
